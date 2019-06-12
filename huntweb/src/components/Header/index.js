1530d4bdbf0adaaccaef98816e70e20465dda8dd
Thalys02 <Thalys02@gmail.com>
2019-06-12 09:44:14 -0300

    Projeto HuntWeb com React

---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* São iguais!!
+class Header extends Component{
+    render(){
+        return <h1>Hello</h1>
+    }
+}*/
+
+import './style.css';
+
+const Header = () => <header id="main-header">JSHunt</header>;
+
+export default Header;
